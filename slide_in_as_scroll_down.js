130d7db40671c2587c1baef0a547d8eab50c0915
Sophy Pal <sophy.pal@cyaninc.com>
2014-09-18 20:47:47 -0700

    Vendor prefix galore and onload handler

--- slide_in_as_scroll_down.js
+++ slide_in_as_scroll_down.js
@@ -28,24 +28,27 @@
     
 })(jQuery);
 
-var win = $(window);
+$(document).ready(function () {
 
-var allMods = $(".module");
+    var win = $(window);
 
-allMods.each(function(i, el) {
-  var el = $(el);
-  if (el.visible(true)) {
-    el.addClass("already-visible"); 
-  } 
-});
+    var allMods = $(".module");
 
-win.scroll(function(event) {
-  
-  allMods.each(function(i, el) {
-    var el = $(el);
-    if (el.visible(true)) {
-      el.addClass("come-in"); 
-    } 
-  });
-  
+    allMods.each(function(i, el) {
+      var el = $(el);
+      if (el.visible(true)) {
+        el.addClass("already-visible"); 
+      } 
+    });
+
+    win.scroll(function(event) {
+      
+      allMods.each(function(i, el) {
+        var el = $(el);
+        if (el.visible(true)) {
+          el.addClass("come-in"); 
+        } 
+      });
+      
+    });
 });
